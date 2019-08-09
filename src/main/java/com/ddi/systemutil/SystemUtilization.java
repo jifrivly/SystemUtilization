cf606b1039a13679f41dc75f9433dfae4a5fe347
JIFRI <jifri.vly@vuelogix.com>
2019-08-09 19:54:46 +0530

    neee

--- src/main/java/com/ddi/systemutil/SystemUtilization.java
+++ src/main/java/com/ddi/systemutil/SystemUtilization.java
@@ -1,5 +1,6 @@
 package com.ddi.systemutil;
 
+import java.io.File;
 import java.lang.management.ManagementFactory;
 import java.util.Iterator;
 import java.util.PriorityQueue;
@@ -7,16 +8,16 @@ import java.util.Queue;
 
 import com.sun.management.OperatingSystemMXBean;
 
-
 /**
  * SystemUtilization
  */
 public class SystemUtilization {
 
     public static OperatingSystemMXBean osMxBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
-    
+
     public static Queue<Double> cpuLoadsQueue = new PriorityQueue<Double>();
 
+    File storage = new File("/");
 
     public static void main(String[] args) {
 
@@ -38,20 +39,40 @@ public class SystemUtilization {
 
         }
 
-
-
-        
     }
 
-    public void getValues(){
-        if (cpuLoadsQueue.size() > 40) {
-            Double sum = 0.0;
-            while (queue.hasNext()) {
-                sum += queue.next();
-            }
-            Double avg = sum / 40;
+    public void getValues() {
+        
+        // All returning values declaration
+        double currentCpu;
+        double totalRam;
+        double usedRam;
+        long totalStorage;
+        long freeStorage;
+        long rateOfWriting;
 
-            System.out.print(avg);
+        // Calculation for Cpu Utilization
+        // Here we take current 40 values (all values) from queue and calculate average  
+        Iterator<Double> queue = cpuLoadsQueue.iterator();
+
+        double sum = 0.0;
+        while (queue.hasNext()) {
+            sum += queue.next();
         }
-}
+        double avg = sum / 40;
+
+        currentCpu = avg;
+
+
+        // Calculation for Ram Utilization
+        totalRam = osMxBean.getTotalPhysicalMemorySize();
+        usedRam = totalRam - osMxBean.getFreePhysicalMemorySize();
+
+        // Calculation for Storage Utilization
+        totalStorage = storage.getTotalSpace();
+        freeStorage = storage.getFreeSpace();
+
+
+
+    }
 }
